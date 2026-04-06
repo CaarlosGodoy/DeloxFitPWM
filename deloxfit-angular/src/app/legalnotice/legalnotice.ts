d94f8e451abf907f40739118a736685345a2b4c2
marcpear <marcospearm@gmail.com>
2026-04-06 20:16:05 +0000

    Multiple changes

---
+++ deloxfit-angular/src/app/legalnotice/legalnotice.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legalnotice',
+  imports: [],
+  templateUrl: './legalnotice.html',
+  styleUrl: './legalnotice.css',
+})
+export class Legalnotice {}
