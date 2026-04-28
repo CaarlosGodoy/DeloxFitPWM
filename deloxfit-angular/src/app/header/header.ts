58a929f03a9da88084521fd3b99ffd84ad7e2b5c
Pauu02 <paulasangar05@gmail.com>
2026-04-28 11:57:17 +0100

    Bootstrap

--- deloxfit-angular/src/app/header/header.ts
+++ deloxfit-angular/src/app/header/header.ts
@@ -1,20 +1,39 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { NgClass } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   authService = inject(AuthService);
-  menuActive = signal(false);
 
-  toggleMenu() {
-    this.menuActive.update(v => !v);
+  constructor(private router: Router) {}
+
+  goToSection(section: string) {
+    this.router.navigate(['/']).then(() => {
+      setTimeout(() => {
+        const el = document.getElementById(section);
+
+        if (el) {
+          const offset = 90;
+
+          const y =
+            el.getBoundingClientRect().top +
+            window.pageYOffset -
+            offset;
+
+          window.scrollTo({
+            top: y,
+            behavior: 'smooth'
+          });
+        }
+      }, 100);
+    });
   }
 }
